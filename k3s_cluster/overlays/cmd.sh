bfc45ffa0101a3fbd1ed3eb753b3be1f81097890
glados-creator <79933806+glados-creator@users.noreply.github.com>
2026-06-17 00:54:52 +0200

    add sso auth test + forgot

--- k3s_cluster/overlays/cmd.sh
+++ k3s_cluster/overlays/cmd.sh
@@ -1,3 +1,5 @@
+set -x
+
 echo "kubectl apply -k ./production"
 echo "kubectl delete deployment,cm,secret,ingress,pvc --all -n <namespace>"
 echo "kubectl delete namespace <namespace>"
@@ -8,60 +10,59 @@ echo "/home/k3su/infra/k3s_cluster/overlays/production # cat /etc/exports"
 echo "/bhole 192.168.0.0/16(rw,async,wdelay,all_squash,anonuid=1000,anongid=1000,subtree_check)"
 echo "/home/k3su/infra/k3s_cluster/overlays/production # exportfs -rav"
 
-mkdir /bhole/cluster/default
-mkdir /bhole/cluster/default/technitium
+mkdir -p /bhole/cluster/default
+mkdir -p /bhole/cluster/default/technitium
 
 
-mkdir /bhole/cluster/private
-mkdir /bhole/cluster/private/dozzle
-mkdir /bhole/cluster/private/gatus
-mkdir /bhole/cluster/private/code/webui
+mkdir -p /bhole/cluster/private
+mkdir -p /bhole/cluster/private/dozzle
+mkdir -p /bhole/cluster/private/gatus
+mkdir -p /bhole/cluster/private/code/webui
 
-mkdir /bhole/cluster/public
-mkdir /bhole/cluster/public/gatus
+mkdir -p /bhole/cluster/public
+mkdir -p /bhole/cluster/public/gatus
 
-mkdir /bhole/cluster/dmz
-mkdir /bhole/cluster/dmz/gatus
-mkdir /bhole/cluster/dmz/media-stack/
-mkdir /bhole/cluster/dmz/media-stack/media
-mkdir /bhole/cluster/dmz/media-stack/media/movies
-mkdir /bhole/cluster/dmz/media-stack/media/tv
-mkdir /bhole/cluster/dmz/media-stack/media/music
-mkdir /bhole/cluster/dmz/media-stack/media/books
-mkdir /bhole/cluster/dmz/media-stack/media/comics
-mkdir /bhole/cluster/dmz/media-stack/media/movies
-mkdir /bhole/cluster/dmz/media-stack/downloads
-mkdir /bhole/cluster/dmz/media-stack/downloads/radarr
-mkdir /bhole/cluster/dmz/media-stack/downloads/sonarr
-mkdir /bhole/cluster/dmz/media-stack/downloads/lidarr
-mkdir /bhole/cluster/dmz/media-stack/downloads/readarr
-mkdir /bhole/cluster/dmz/media-stack/downloads/kapowarr
-mkdir /bhole/cluster/dmz/media-stack/downloads/qbittorrent
-mkdir /bhole/cluster/dmz/media-stack/downloads/unpackerr
-mkdir /bhole/cluster/dmz/media-stack/jellyfin/config
-mkdir /bhole/cluster/dmz/media-stack/jellyfin/cache
-mkdir /bhole/cluster/dmz/media-stack/radarr/config
-mkdir /bhole/cluster/dmz/media-stack/sonarr/config
-mkdir /bhole/cluster/dmz/media-stack/lidarr/config
-mkdir /bhole/cluster/dmz/media-stack/readarr/config
-mkdir /bhole/cluster/dmz/media-stack/kapowarr/db
-mkdir /bhole/cluster/dmz/media-stack/prowlarr/config
-mkdir /bhole/cluster/dmz/media-stack/jellyseerr/config
-mkdir /bhole/cluster/dmz/media-stack/qbittorrent/config
-mkdir /bhole/cluster/dmz/media-stack/tdarr/server
-mkdir /bhole/cluster/dmz/media-stack/tdarr/configs
-mkdir /bhole/cluster/dmz/media-stack/tdarr/logs
-mkdir /bhole/cluster/dmz/media-stack/tdarr/temp
-mkdir /bhole/cluster/dmz/media-stack/bazarr/config
-mkdir /bhole/cluster/dmz/media-stack/autobrr/config
-mkdir /bhole/cluster/dmz/media-stack/jellystat/data
-mkdir /bhole/cluster/dmz/media-stack/unpackerr/config
-mkdir /bhole/cluster/dmz/media-stack/profilarr/config
-mkdir /bhole/cluster/dmz/media-stack/youtarr/config
+mkdir -p /bhole/cluster/dmz
+mkdir -p /bhole/cluster/dmz/gatus
+mkdir -p /bhole/cluster/dmz/media-stack/
+mkdir -p /bhole/cluster/dmz/media-stack/media
+mkdir -p /bhole/cluster/dmz/media-stack/media/movies
+mkdir -p /bhole/cluster/dmz/media-stack/media/tv
+mkdir -p /bhole/cluster/dmz/media-stack/media/music
+mkdir -p /bhole/cluster/dmz/media-stack/media/books
+mkdir -p /bhole/cluster/dmz/media-stack/media/comics
+mkdir -p /bhole/cluster/dmz/media-stack/downloads
+mkdir -p /bhole/cluster/dmz/media-stack/downloads/radarr
+mkdir -p /bhole/cluster/dmz/media-stack/downloads/sonarr
+mkdir -p /bhole/cluster/dmz/media-stack/downloads/lidarr
+mkdir -p /bhole/cluster/dmz/media-stack/downloads/readarr
+mkdir -p /bhole/cluster/dmz/media-stack/downloads/kapowarr
+mkdir -p /bhole/cluster/dmz/media-stack/downloads/qbittorrent
+mkdir -p /bhole/cluster/dmz/media-stack/downloads/unpackerr
+mkdir -p /bhole/cluster/dmz/media-stack/jellyfin/config
+mkdir -p /bhole/cluster/dmz/media-stack/jellyfin/cache
+mkdir -p /bhole/cluster/dmz/media-stack/radarr/config
+mkdir -p /bhole/cluster/dmz/media-stack/sonarr/config
+mkdir -p /bhole/cluster/dmz/media-stack/lidarr/config
+mkdir -p /bhole/cluster/dmz/media-stack/readarr/config
+mkdir -p /bhole/cluster/dmz/media-stack/kapowarr/db
+mkdir -p /bhole/cluster/dmz/media-stack/prowlarr/config
+mkdir -p /bhole/cluster/dmz/media-stack/jellyseerr/config
+mkdir -p /bhole/cluster/dmz/media-stack/qbittorrent/config
+mkdir -p /bhole/cluster/dmz/media-stack/tdarr/server
+mkdir -p /bhole/cluster/dmz/media-stack/tdarr/configs
+mkdir -p /bhole/cluster/dmz/media-stack/tdarr/logs
+mkdir -p /bhole/cluster/dmz/media-stack/tdarr/temp
+mkdir -p /bhole/cluster/dmz/media-stack/bazarr/config
+mkdir -p /bhole/cluster/dmz/media-stack/autobrr/config
+mkdir -p /bhole/cluster/dmz/media-stack/jellystat/data
+mkdir -p /bhole/cluster/dmz/media-stack/unpackerr/config
+mkdir -p /bhole/cluster/dmz/media-stack/profilarr/config
+mkdir -p /bhole/cluster/dmz/media-stack/youtarr/config
 
 sudo chown -R 1000:1000 /bhole/cluster/*
 
-kubectl rollout restart deployment -n prod-dmz dmz-media-qbittorrent-deployment
+# kubectl rollout restart deployment -n prod-dmz dmz-media-qbittorrent-deployment
 
 
 # rm -rf /var/log/calico                                                                                                                                   
